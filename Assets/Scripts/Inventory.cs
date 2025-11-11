1ad930f663a7a061c7b612af286e17eed7c1dc73
matheusdev200 <62211090+matheusdev200@users.noreply.github.com>
2025-11-11 19:12:49 -0300

    Exemplo de Start IEnumerator

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class Inventory : MonoBehaviour //C# é fortemente Tipado
 {
     public string[] itens;
+    public GameObject coisa;
     //public int[] quantidadeDeItens;
 
     //listagem de coisas 
@@ -16,7 +18,31 @@ public class Inventory : MonoBehaviour //C# é fortemente Tipado
         {
             Debug.Log(itens[variavelDeContagem]);//0 //1 //2 // 3 //4! ponto de parada
         }
-    }
+    }//feedback e gamefeel
+    //IEnumerator Start()//Coroutine
+    //{
+    //    //Threading e AsyncOperation -> deixa lá :)
+
+    //    //Aloca memória de trabalho
+    //    //Aloca processamento
+    //    //método paralelo que vai rodar no mainThread* curiosidade
+
+    //    //obriga a existência de uma operação de suspensão
+    //    //yield return new WaitForSeconds(0.1f);
+    //    yield return new WaitForSeconds(0.1f);
+    //    yield return new WaitUntil(()=> coisa.activeInHierarchy == true);
+    //    //Desativa o inventário depois do delay
+
+    //    //variavel do contador ;    critério (enquanto for) de encerramento ;  critério de avanço da repetição
+    //    for (int variavelDeContagem = 0; variavelDeContagem < itens.Length; variavelDeContagem++) //for, while, switch "laços de repetição"
+    //    {
+    //        Debug.Log(itens[variavelDeContagem]);//0 //1 //2 // 3 //4! ponto de parada
+    //    }
+
+    //}
+
+    //
+
     /*
         itens {"Mochila", "Picareta", "Pá", "Lanterna"}  (4 elementos, o maior índice é 3)
         itens[0]
